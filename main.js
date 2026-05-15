9c4590536451385091be871ffc11a758903487ec
adi-panwar <panwaraditya759@gmail.com>
2026-05-16 02:26:39 +0530

    add some logic

--- main.js
+++ main.js
@@ -8,26 +8,35 @@ let computerNumber = Math.floor(Math.random() * 100)
 
 const runProgram = (e) => {
     e.preventDefault()
-
+    // const userGuess =  parseInt(input.value)
     const userGuess = input.valueAsNumber
 
     if (userGuess === computerNumber) {
-        p.innerText = "🎉 You are right!"
+        p.innerText = "🥰 You are right!"
         p.className = "p-3 mb-4 font-semibold text-lg rounded-lg bg-green-500/80 text-white"
-        body.className = "min-h-screen flex items-center justify-center bg-gradient-to-br from-green-400 to-emerald-600 transition-all duration-500"
+        body.className = "min-h-screen flex items-center justify-center bg-gradient-to-br from-green-400 to-emerald-600 transition-all duration-500 "
         computerNumber = Math.floor(Math.random() * 100)
 
 
     }
     else if (userGuess > computerNumber) {
         p.innerText = "⬇️ Guess lower number"
-        p.className = "p-3 mb-4 font-semibold text-lg rounded-lg bg-yellow-400/80 text-black"
-        body.className = "min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-400 to-red-500 transition-all duration-500"
+        p.className = "p-3 mb-4 font-semibold text-lg rounded-lg bg-yellow-400/80 text-black  "
+
+        p.classList.add("scale-110")
+        setTimeout(() => {
+            p.classList.remove("scale-110")
+        }, 200)
+        body.className = "min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-400 to-red-500 transition-all duration-500 "
 
     }
     else {
         p.innerText = "⬆️ Guess higher number"
         p.className = "p-3 mb-4 font-semibold text-lg rounded-lg bg-blue-500/80 text-white"
+        p.classList.add("scale-110")
+        setTimeout(() => {
+            p.classList.remove("scale-110")
+        }, 200)
         body.className = "min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-400 to-purple-500 transition-all duration-500"
 
     }
